2d1cc0ea537509dd0e2abcef872f43585ed41a3b
Rod <burich.developer@gmail.com>
2021-01-14 15:36:22 -0700

    Create Artist Controller with DDl query. Add filter search query to AlbumController. VM class SelectionList

--- ChinookDemoSolution/ChinookSystem/BLL/AlbumController.cs
+++ ChinookDemoSolution/ChinookSystem/BLL/AlbumController.cs
@@ -9,8 +9,8 @@ using ChinookSystem.Entities;
 using ChinookSystem.DAL;
 using ChinookSystem.ViewModels;
 using System.ComponentModel;
-
 #endregion
+
 namespace ChinookSystem.BLL
 {
     [DataObject]
@@ -25,6 +25,24 @@ namespace ChinookSystem.BLL
             {
                 //Linq to Entity
                 IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name
+                                                    };
+                return results.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ArtistAlbums> Albums_GetAlbumsForArtists(int artistid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                //Linq to Entity
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    where x.ArtistId == artistid
                                                     select new ArtistAlbums
                                                     {
                                                         Title = x.Title,
---
+++ ChinookDemoSolution/ChinookSystem/ViewModels/SelectionList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    //this class will be used as a generic container for data that 
+    // will load a dropdown list
+    //this value field will represent an integer PK 
+    //the display field will represent the display string of the DDL
+    public class SelectionList
+    {
+        public int ValueField { get; set; }
+        public string DisplayField { get; set; }
+    }
+}
